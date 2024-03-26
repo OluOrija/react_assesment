58166b928d043ca010eb3dc39ca2a8820de5ee77
Olusegun Orija <oluorija@gmail.com>
2024-03-26 18:29:28 +0000

    v2
    implemented styling for the data retrieved
    implemented error handling
    - logging to backend
    - and error message management for the ui

---
+++ react-api-app/src/constants/images.js
@@ -0,0 +1,7 @@
+import api from '../assets/api.png';
+
+const images = {
+  api,
+};
+
+export default images;
--- react-api-app/src/container/PostsList/PostsList.jsx
+++ react-api-app/src/container/PostsList/PostsList.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './PostsList.css';
+import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
+import Button from 'react-bootstrap/Button';
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
@@ -9,7 +12,7 @@ const PostList = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const response = await fetch('https://jsonplaceholder.typicode.com/postsFail');
         if (!response.ok) {
           throw new Error('Failed to fetch posts');
         }
@@ -17,7 +20,8 @@ const PostList = () => {
         setPosts(data);
         setLoading(false);
       } catch (error) {
-        setError(error.message);
+        console.error('Error fetching posts:', error); // Log error to console //ideally a backend logging mechanism somewhere
+        setError('Oops! Something went wrong. Please try again later.'); // Set user-friendly error message        
         setLoading(false);
       }
     };
@@ -25,20 +29,28 @@ const PostList = () => {
     fetchPosts();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  const handleButtonClick = (postId) => {
+    alert(`Clicked button for post ID: ${postId}`);
+  };
+
+  if (loading) return <Spinner animation="border" role="status"><span className="sr-only">Loading...</span></Spinner>;
+  if (error) return <div>Error: {error}</div>; // Display user-friendly error message
 
   return (
     <div>
-      <h1>Posts</h1>
-      <ul>
+      <h1 className="text-center">Posts</h1>
+      <div className="card-container">
         {posts.map(post => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </li>
+            <Card style={{ width: '18rem' }}>
+            <Card.Body className="bg-light">
+                <Card.Title className="text-primary">{post.title}</Card.Title>
+                <Card.Text>{post.body}</Card.Text>
+                <Button variant="primary" onClick={() => handleButtonClick(post.id)}>Click me</Button>
+            </Card.Body>
+            </Card>
+            
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
